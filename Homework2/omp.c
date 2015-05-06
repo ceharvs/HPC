cad710a33676071ac004595be332512bcf5602e0
Christine Harvey <ceharvs@gmail.com>
2015-05-06 18:33:41 -0500

    Updated vector field results

--- Homework2/omp.c
+++ Homework2/omp.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
 double func( double x, double y, double a, double b) {
 	return a*x + b*(x)/(x*x+y*y);
@@ -9,24 +10,24 @@ double func( double x, double y, double a, double b) {
 
 int main(int argc,char **argv)
 {
-	int ix, iy, nx, ny, maxX, maxY;
-	double a, b, deltax, deltay, maxMAG, *arr, *gradx, *grady, *magnitude, x0, y0;
+	int ix, iy, nx, ny;
+	double a, b, deltax, deltay, maxMAG, *arr, *gradx, *grady, *magnitude, x0, y0, maxX, maxY;
         double x,y;
 	FILE *fp;
   
 	/* Declare Variables */
 	a = 0.5;
-	b = 3.25;
-	deltax = 0.01;
-	deltay = 0.01;
-	x0 = -0.5;
-	y0 = -0.5;
+	b = 0.15;
+	deltax = 0.002;
+	deltay = 0.002;
+	x0 = -0.1;
+	y0 = -0.1;
 	nx = 100;
 	ny = 100;
   
 	/* Find the maximum magnitude */
-	maxX = 0;
-	maxY = 0;
+	maxX = 0.0;
+	maxY = 0.0;
 	maxMAG = 0.0;
 	
 	/* 2D array declaration */
@@ -36,44 +37,50 @@ int main(int argc,char **argv)
 	magnitude = malloc(nx*ny*sizeof(double));
   
 	/* Set up initial array values */
-	#pragma omp parallel for private (ix, iy) shared(nx, ny, a, b) 
+	/*#pragma omp parallel for private (ix, iy, x, y) shared(nx, ny, a, b)*/
 	for(ix=0; ix<nx; ix++){
             x=x0+ix*deltax;
 		for(iy=0; iy<ny; iy++){
                     y=y0+iy*deltay;
-			arr[nx*ix+iy] = func(x, y, a, b);
+		    arr[nx*ix+iy] = func(x, y, a, b);
+		    /*printf("%f \t%f\t%f\n", x,y,arr[nx*ix+iy]);*/
 		}
 	}
 	
 	/* Compute the X and Y gradients */
-	/* 	grad[ix][iy] = (arr[ix+1][jx+1]-arr[ix+1][iy-1]
-	-arr[ix-1][iy+1]+arr[ix-1][iy-1])/(4*deltax*deltay) */
 	
-	/* Print to file in format x,y,gradx,grady */
+	/* Print to file in format x,y,z,gradx,grady */
 	fp = fopen("output.csv","w");
 	fprintf(fp,"x,y,z,gradx,grady\n");
 		 
-	#pragma omp parallel for private (ix, iy) shared(nx, ny, deltax, deltay) 
+	/*#pragma omp parallel for private (ix, iy) shared(nx, ny, deltax, deltay) */
 	for(ix=1; ix<nx-1; ix++){
+	        x=x0+ix*deltax;
 		for(iy=1; iy<ny-1; iy++){
+                    	y=y0+iy*deltay;
 			gradx[nx*ix+iy] = (arr[nx*(ix+1)+iy]-arr[nx*(ix-1)+iy])/(2*deltax);
+		        /*printf("Grad X at (%f,%f) = %f\n", x,y,gradx[nx*ix+iy]);*/
 			grady[nx*ix+iy] = (arr[nx*ix+iy+1]-arr[nx*ix+iy-1])/(2*deltay);
 			magnitude[nx*ix+iy] = sqrt(pow(gradx[nx*ix+iy],2) +pow(grady[nx*ix+iy],2) );
+		        /*printf("Grad Y at (%f,%f) = %f\n", x,y,grady[nx*ix+iy]);*/
 		}
 	}
 	
 	for(ix=1; ix<nx-1; ix++){
+	        x=x0+ix*deltax;
 		for(iy=1; iy<ny-1; iy++){
-			fprintf(fp, "%f,%f,0,%f,%f\n",x0+ix*deltax,y0+iy*deltay,gradx[nx*ix+iy],grady[nx*ix+iy]);
+                    	y=y0+iy*deltay;
+			fprintf(fp, "%f,%f,0,%f,%f\n",x,y,gradx[nx*ix+iy],grady[nx*ix+iy]);
 			if(magnitude[nx*ix+iy] > maxMAG){
 				maxMAG = magnitude[nx*ix+iy];
-				maxX = ix;
-				maxY = iy;
+				maxX = x;
+				maxY = y;
+				/*printf("max is at (%f, %f) = %f\n", maxX, maxY, maxMAG);*/
 			}
 		}
 	}
 	
-	printf("Maximum magnitude is at (%d, %d) and is %f.\n", maxX, maxY, maxMAG);
+	printf("Maximum magnitude is at (%f, %f) and is %f.\n", maxX, maxY, maxMAG);
 
 	free(arr);
 	free(gradx);
